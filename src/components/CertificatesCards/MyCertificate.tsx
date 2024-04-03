afbb24529d95a88a3f6790fb0389f0b9111855de
ashiqee <ashiq.buet73@gmail.com>
2024-04-03 16:37:57 +0600

    certificate condition done

--- src/components/CertificatesCards/MyCertificate.tsx
+++ src/components/CertificatesCards/MyCertificate.tsx
@@ -8,20 +8,25 @@ import {
 import Image from "next/image";
 
 import certificateImage from "@/assests/Images/Frame 1173.png";
-import { BiCertification } from "react-icons/bi";
+import certificateYellowImage from "@/assests/Images/yellow.png";
+import certificationSVG from "@/assests/icon/Group 161.png";
+import { useState } from "react";
+import { AiOutlineDownload } from "react-icons/ai";
+import { BsShare } from "react-icons/bs";
 
 export default function MyCertificates() {
+	const [activeCetificate, setActiveCertificate] = useState(1);
 	const certificatesData = [
 		{
 			id: 1,
-			certificateName: "Yellow Belt Certificate",
+			certificateName: "White Belt Certificate",
 			achievmentDate: "5 Apr 2024",
 			details:
 				"non, amet, nibh ullamcorper ex. laoreet amet,  leo. at,  tincidunt enim. facilisis amet, urna. odio odio non urna. nibh id eu nisi .",
 			certificateIcon: "",
 		},
 		{
-			id: 1,
+			id: 2,
 			certificateName: "Yellow Belt Certificate",
 			achievmentDate: "5 Apr 2024",
 			details:
@@ -33,24 +38,40 @@ export default function MyCertificates() {
 		<>
 			<div className="md:flex gap-4 ">
 				{/* certificate details  */}
-				<div className="space-y-4 max-w-[30vw]">
+				<div className="space-y-4 md:max-w-[30vw]">
 					{certificatesData?.map((data) => (
-						<Card className="max-h-60 text-blue-950 rounded-3xl" key={data.id}>
-							<div className="flex justify-items-start border-2 gap-1 ">
+						<Card
+							onClick={() => setActiveCertificate(data.id)}
+							className={
+								activeCetificate === data.id
+									? "md:max-h-60 hover:bg-[#eaeafb] text-blue-950 rounded-3xl"
+									: "md:max-h-60 bg-[#eaeafb] text-blue-950 rounded-3xl"
+							}
+							key={data.id}
+						>
+							<div className="flex justify-items-start  gap-1 ">
 								<div className="text-2xl py-6 pl-5">
-									<BiCertification />
+									<Image
+										src={certificationSVG}
+										alt="Certification Icon"
+										width={70}
+									/>
 								</div>
 								<div>
 									<CardHeader>
-										<CardTitle className="font-normal pb-5">
+										<CardTitle className="font-normal flex items-center justify-between pb-5">
 											{data.certificateName}
+											<div className="text-xl text-blue-950 relative z-10 right-0 flex gap-3  pr-0">
+												<BsShare />
+												<AiOutlineDownload />
+											</div>
 										</CardTitle>
 										<CardDescription>
 											Achievement Date: {data.achievmentDate}
 										</CardDescription>
 									</CardHeader>
 									<CardContent>
-										<p className="font-normal">{data.details}</p>
+										<p className="font-normal text-justify">{data.details}</p>
 									</CardContent>
 								</div>
 							</div>
@@ -58,14 +79,23 @@ export default function MyCertificates() {
 					))}
 				</div>
 				{/* certificate views  */}
-				<Card className="rounded-3xl">
+				<Card className="rounded-3xl mt-10 md:mt-0">
 					<CardContent>
-						<Image
-							className="p-2 px-6"
-							src={certificateImage}
-							alt=""
-							width={700}
-						/>
+						{activeCetificate === 1 ? (
+							<Image
+								className="p-2 px-6"
+								src={certificateImage}
+								alt=""
+								width={700}
+							/>
+						) : (
+							<Image
+								className="p-2 px-6"
+								src={certificateYellowImage}
+								alt=""
+								width={700}
+							/>
+						)}
 					</CardContent>
 				</Card>
 			</div>
--- src/components/CertificatesCards/Transcript.tsx
+++ src/components/CertificatesCards/Transcript.tsx
@@ -1,11 +1,102 @@
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
+import { useState } from "react";
+
+import transcriptsImage from "@/assests/Images/Transcripts.png";
+import certificationSVG from "@/assests/icon/Group 161.png";
+
+import { AiOutlineDownload } from "react-icons/ai";
+import { BsShare } from "react-icons/bs";
 export default function Transcripts() {
+	const [activeTranscript, setActiveTranscript] = useState(1);
+	const transcriptsData = [
+		{
+			id: 1,
+			transcriptsName: "White Belt Transcript",
+			achievmentDate: "5 Apr 2024",
+			details:
+				"non, amet, nibh ullamcorper ex. laoreet amet,  leo. at,  tincidunt enim. facilisis amet, urna. odio odio non urna. nibh id eu nisi .",
+			transcriptIcon: "",
+		},
+		{
+			id: 2,
+			transcriptsName: "Yellow Belt Transcript",
+			achievmentDate: "5 Apr 2024",
+			details:
+				"non, amet, nibh ullamcorper ex. laoreet amet,  leo. at,  tincidunt enim. facilisis amet, urna. odio odio non urna. nibh id eu nisi .",
+			transcriptIcon: "",
+		},
+	];
 	return (
 		<>
-			<div>
+			<div className="md:flex gap-4 ">
 				{/* certificate details  */}
-				<div>Trans Belt</div>
+				<div className="space-y-4 md:max-w-[30vw]">
+					{transcriptsData?.map((data) => (
+						<Card
+							onClick={() => setActiveTranscript(data.id)}
+							className={
+								activeTranscript === data.id
+									? "md:max-h-60 hover:bg-[#eaeafb] text-blue-950 rounded-3xl"
+									: "md:max-h-60 bg-[#eaeafb] text-blue-950 rounded-3xl"
+							}
+							key={data.id}
+						>
+							<div className="flex justify-items-start  gap-1 ">
+								<div className="text-2xl py-6 pl-5">
+									<Image
+										src={certificationSVG}
+										alt="Certification Icon"
+										width={70}
+									/>
+								</div>
+								<div>
+									<CardHeader>
+										<CardTitle className="font-normal flex items-center justify-between pb-5">
+											{data.transcriptsName}
+											<div className="text-xl text-blue-950 relative z-10 right-0 flex gap-3  pr-0">
+												<BsShare />
+												<AiOutlineDownload />
+											</div>
+										</CardTitle>
+										<CardDescription>
+											Achievement Date: {data.achievmentDate}
+										</CardDescription>
+									</CardHeader>
+									<CardContent>
+										<p className="font-normal text-justify">{data.details}</p>
+									</CardContent>
+								</div>
+							</div>
+						</Card>
+					))}
+				</div>
 				{/* certificate views  */}
-				<div></div>
+				<Card className="rounded-3xl mt-10 md:mt-0">
+					<CardContent>
+						{activeTranscript === 1 ? (
+							<Image
+								className="p-2 px-6"
+								src={transcriptsImage}
+								alt=""
+								width={700}
+							/>
+						) : (
+							<Image
+								className="p-2 px-6"
+								src={transcriptsImage}
+								alt=""
+								width={700}
+							/>
+						)}
+					</CardContent>
+				</Card>
 			</div>
 		</>
 	);
